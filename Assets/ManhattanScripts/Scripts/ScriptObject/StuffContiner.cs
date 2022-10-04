683237681f83b4a7e581a22afa08e1c9d5f44345
erfan <erfansh@joyixir.com>
2022-10-04 14:45:12 +0330

    import scripts

---
+++ Assets/ManhattanScripts/Scripts/ScriptObject/StuffContiner.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "StuffContiner", menuName = "Diaco/StuffContiner", order = 1)]
+public class StuffContiner : ScriptableObject
+{
+
+    public List<Diaco.Manhatan.Structs.StuffContinerData> stuffs;
+    
+
+}
