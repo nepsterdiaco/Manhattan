189a4e5269fc9dd2cd44edf3f98b3633c18b4e7e
erfan <erfansh@joyixir.com>
2022-10-20 19:46:07 +0330

    add scripts

--- Assets/Scripts/Componets/StuffObject.cs
+++ Assets/Scripts/Componets/StuffObject.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 
@@ -16,5 +17,30 @@ public class StuffObject : MonoBehaviour
     public String GetGroup()
     {
         return Data.group;
-    } 
+    }
+
+    [Header("Editor Utility")] public string StringToOmit;
+    [SerializeField] private bool DoTransformWithRenaming;
+    [Button]
+    public void GetNameFromChildren()
+    {
+        if (DoTransformWithRenaming) SetTransformToZero();
+        var child = gameObject.GetComponentsInChildren<Transform>()[1];
+        var mName = child.name;
+        if (StringToOmit.Length > 0)
+        {
+            var s = mName.Replace(StringToOmit, "");        
+            transform.name = s;
+            return;
+        }
+        transform.name = mName;
+    }
+
+    public void SetTransformToZero()
+    {
+        var child = gameObject.GetComponentsInChildren<Transform>()[1];
+        child.position = new Vector3(0, 0, 0);
+        child.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+        // child.localScale = new Vector3(1, 1, 1);
+    }
 }
