d3e66bbf1f738a310e83648efd2dbfff21d968ea
erfan <erfansh@joyixir.com>
2022-10-22 17:53:44 +0330

    stuff

--- Assets/Scripts/Editor/SelectionOffseter.cs
+++ Assets/Scripts/Editor/SelectionOffseter.cs
@@ -7,15 +7,27 @@ class SelectionOffseter : EditorWindow
 
     class SelectionActiveTransform : ScriptableObject
     {
-        [MenuItem("Example/Duplicate at Origin _d")]
-        static void DuplicateSelected()
+        [MenuItem("Example/Offset Selection _d")]
+        static void OffsetSelected()
         {
+            transformOffset = 0;
+            int offset = 0;
+            var name = EditorInputDialog.Show("Question", "Please enter your name", "");
+            if (!string.IsNullOrEmpty(name))
+            {
+                offset = ConvertToInt(name);
+            }
+            else
+            {
+                return;
+            }
+
             var selection = Selection.transforms;
             foreach (var selected in selection)
             {
                 var tf = selected.transform;
                 selected.Translate(Vector3.right * transformOffset);
-                transformOffset += 50f;
+                transformOffset += offset;
             }
         }
     }
@@ -25,5 +37,20 @@ class SelectionOffseter : EditorWindow
     static bool ValidateSelection()
     {
         return Selection.activeTransform != null;
+    }
+
+    public static int ConvertToInt(string inputString)
+    {
+        int result;
+        string input;
+        if (int.TryParse(inputString, out result))
+        {
+            return result;
+        }
+        else
+        {
+            Debug.Log("Not a valid int");
+            return 0;
+        }
     }
 }
